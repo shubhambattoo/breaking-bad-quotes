bb69f058a32a4ce34f90216b16b569d667f65e68
shubhambattoo <shubham.battoo@hotmail.com>
2020-09-20 19:01:29 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Quote from './components/Quote';
 
@@ -8,6 +9,7 @@ function App() {
     <main className="container">
       <Header />
       <Quote />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      &copy; 2020. <a href="https://shubhambattoo.in">Shubham Battoo</a>
+    </footer>
+  );
+};
+
+export default Footer;
